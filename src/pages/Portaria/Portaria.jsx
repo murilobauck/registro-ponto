56e642cbcf14c2be1780015388ac62ceeccf2d20
murilobauck <murilobauck2@gmail.com>
2025-09-25 17:06:10 -0300

    chore higher time

--- src/pages/Portaria/Portaria.jsx
+++ src/pages/Portaria/Portaria.jsx
@@ -120,7 +120,7 @@ function Portaria() {
 
   const handleCaptureSuccess = (message) => {
     setMainStatus({ message, type: 'success' });
-    setTimeout(() => setMainStatus({ message: '', type: '' }), 4000);
+    setTimeout(() => setMainStatus({ message: '', type: '' }), 6500);
   };
 
   const dateOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
