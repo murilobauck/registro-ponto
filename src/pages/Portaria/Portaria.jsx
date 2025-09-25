b90173774c64da691edff4f198e4b75f8960b2e8
murilobauck <murilobauck2@gmail.com>
2025-09-25 14:17:26 -0300

    fix url base

--- src/pages/Portaria/Portaria.jsx
+++ src/pages/Portaria/Portaria.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import 'boxicons/css/boxicons.min.css';
 import styles from './Portaria.module.css';
 
-const API_BASE_URL = "https://annette-seminomadic-arctically.ngrok-free.dev";
+const API_BASE_URL = "http://127.0.0.1:5000";
 
 function RecognitionModal({ isOpen, onClose, onCaptureSuccess }) {
   const [status, setStatus] = useState({ message: 'Posicione seu rosto no centro.', type: 'info' });
--- src/pages/Rh/Rh.jsx
+++ src/pages/Rh/Rh.jsx
@@ -15,7 +15,7 @@ const DEPARTAMENTOS = [
   "Pesquisa e Desenvolvimento (P&D)",
 ];
 
-const API_BASE_URL = "https://annette-seminomadic-arctically.ngrok-free.dev";
+const API_BASE_URL = "http://127.0.0.1:5000";
 
 function GestaoView({ onAddNew }) {
   const [funcionarios, setFuncionarios] = useState([]);
