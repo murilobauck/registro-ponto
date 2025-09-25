91c9a284801303ba9e36fd0736ee9077c9432486
murilobauck <murilobauck2@gmail.com>
2025-09-25 04:08:24 -0300

    chore refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Portaria from './pages/Portaria';
-import Rh from './pages/Rh';
+import Portaria from './pages/Portaria/Portaria';
+import Rh from './pages/Rh/Rh';
 
 function App() {
   return (
